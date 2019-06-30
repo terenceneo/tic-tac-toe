f35e0775dfaad8ad0f405f4c812c275817d57750
Terence Neo <terencenyy@yahoo.com.sg>
2019-06-30 18:19:48 +0800

    passed prop from board to square

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ class Square extends React.Component {
   render() {
     return (
       <button className="square">
-        {/* TODO */}
+        {this.props.value}
       </button>
     );
   }
@@ -20,7 +20,7 @@ class Square extends React.Component {
  */
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square />;
+    return <Square value={i}/>;
   }
 
   render() {
